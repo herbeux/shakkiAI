0fbd3c21ee9c2d21ab00ae7b1d885d435729422d
herbeux <hontti04@gmail.com>
2019-02-12 14:08:52 +0200

    Update torni.h

--- torni.h
+++ torni.h
@@ -50,7 +50,34 @@ class Torni : virtual public Nappula
 		}
 
 
+// "ylös" (dy = "delta y", eli y:n muutos)
+		for (int dy = 1; dy <= 7; dy++)
+		{
+			// uudet koordinatit
+			int new_y = x + dy;
 
+			// mennäänkö yli laudan reunasta?
+			if (new_y > 7)
+				break;
+
+			// mitä tutkittavassa ruudussa (new_y, x) on?
+			Nappula* n = asema->_lauta[x][new_y];
+
+			// onko tyhjä ruutu?
+			if (n == nullptr)
+			{
+				lista.push_back(Siirto(Ruutu(x, y), Ruutu(x, new_y)));
+				continue;
+			}
+
+			// onko vastustajan nappula?
+			if (n->getVari() != vari)
+			{
+				lista.push_back(Siirto(Ruutu(x, y), Ruutu(x,new_y)));
+			}
+
+			break;
+		}
 
 
 
