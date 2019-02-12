96a77ddc6da7a5a63759ed083f4c441c48d7ca6d
herbeux <hontti04@gmail.com>
2019-02-12 13:49:54 +0200

    Update nappula.cpp

--- nappula.cpp
+++ nappula.cpp
@@ -11,3 +11,13 @@ Nappula::Nappula(wstring unicode, int vari, int koodi)
 	_vari = vari;
 	_koodi = koodi;
 }
+
+
+// virtual jotta myöhäinen sidonta käytössä
+virtual void annaSiirrot(
+list<Siirto>& lista,
+Ruutu* ruutu,
+Asema* asema,
+int vari
+) = 0;
+
